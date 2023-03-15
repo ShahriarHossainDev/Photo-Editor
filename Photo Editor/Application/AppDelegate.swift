a6c5571228c66a0d34dcb07f6c08e6e00b2a5f7f
ShahriarHossainDev <shahriar.hossain.dev@gmail.com>
2023-03-15 12:01:53 +0600

    AppDelegate

--- Photo Editor/Application/AppDelegate.swift
+++ Photo Editor/Application/AppDelegate.swift
@@ -7,13 +7,18 @@
 
 import UIKit
 
-@main
+@UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
+    var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        self.window = UIWindow(frame: UIScreen.main.bounds)
+        
+        let nav = UINavigationController(rootViewController: HomeViewController())
+        self.window?.rootViewController = nav
+        
+        self.window?.makeKeyAndVisible()
         return true
     }
 
--- Photo Editor/Serecns/Home/View/HomeViewController.swift
+++ Photo Editor/Serecns/Home/View/HomeViewController.swift
@@ -8,11 +8,23 @@
 import UIKit
 
 class HomeViewController: UIViewController {
-
+    
+    @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var getNewPhotoButton: UIButton!
+    @IBOutlet weak var editPhotoButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
-
+    
+    // MARK: - IBAction
+    @IBAction func getNewPhotoButtonAction(_ sender: UIButton) {
+        print("Get New Photo")
+    }
+    
+    @IBAction func editPhotoButtonAction(_ sender: UIButton) {
+        print("Edit Photo")
+    }
 }
